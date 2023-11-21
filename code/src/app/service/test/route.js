ffd66a80fcacec5ea100aaf712b25986f0f3343f
Atlantica <alvisbaran00@gmail.com>
2023-11-21 17:32:33 +0700

    update route.js

--- code/src/app/service/test/route.js
+++ code/src/app/service/test/route.js
@@ -1,25 +1,6 @@
-import User from "@/backend/models/User";
-
+import User from "@/backend/models/user";
 
 export async function GET() {
   const users = await User.findAll();
   return Response.json(users);
-}
-
-export async function POST() {
-  const newUser = User.build({
-    displayName: "User 1",
-    email: "user1",
-    password: "user1",
-  });
-
-  return await newUser.save()
-  .then((res) => {
-    console.info(res)
-    return Response.json( {...res, message: "success" })
-  })
-  .catch(err => {
-    console.info(err)
-    return Response.json({ ...err, message: "error" })
-  });
 }
