1f1d4ca91d51a0656fb444923b387017c171e329
Atlantica <alvisbaran00@gmail.com>
2024-05-21 18:07:46 +0700

    BYPASS AUTH
    + Please send userId as query params to bypass the validator

--- code/src/backend/utils/sessionHandler.js
+++ code/src/backend/utils/sessionHandler.js
@@ -2,33 +2,57 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { getServerSession } from 'next-auth'
 import User from '../models/user'
 
+const BYPASS = true
+
 export async function getUserFromServerSession(req, res) {
   return new Promise(async resolve => {
-    const session = await getServerSession(
-      req,
-      {
-        ...res,
-        getHeader: name => res.headers?.get(name),
-        setHeader: (name, value) => res.headers?.set(name, value)
-      },
-      authOptions
-    )
-    if (!!session && !!session?.user) {
-      const currUser = await User.findByPk(Number(session.user.id), {
-        attributes: ['id', 'cUsername', 'email', 'displayName', 'profilePicture', 'joinDate']
-      })
-      if (!!currUser) {
-        return resolve({
-          user: { ...currUser.dataValues }
+    if (BYPASS) {
+      const searchParams = req.nextUrl.searchParams
+      const userId = searchParams.get('userId') ?? searchParams.get('creatorId') ?? null
+      if (!!userId) {
+        const currUser = await User.findByPk(Number(userId), {
+          attributes: ['id', 'cUsername', 'email', 'displayName', 'profilePicture', 'joinDate']
         })
+        if (!!currUser) {
+          return resolve({
+            user: { ...currUser.dataValues }
+          })
+        }
       }
-    }
 
-    return resolve({
-      error: {
-        code: 401,
-        message: 'Unauthorized!'
+      return resolve({
+        error: {
+          code: 401,
+          message: 'Unauthorized!'
+        }
+      })
+    } else {
+      const session = await getServerSession(
+        req,
+        {
+          ...res,
+          getHeader: name => res.headers?.get(name),
+          setHeader: (name, value) => res.headers?.set(name, value)
+        },
+        authOptions
+      )
+      if (!!session && !!session?.user) {
+        const currUser = await User.findByPk(Number(session.user.id), {
+          attributes: ['id', 'cUsername', 'email', 'displayName', 'profilePicture', 'joinDate']
+        })
+        if (!!currUser) {
+          return resolve({
+            user: { ...currUser.dataValues }
+          })
+        }
       }
-    })
+
+      return resolve({
+        error: {
+          code: 401,
+          message: 'Unauthorized!'
+        }
+      })
+    }
   })
 }
--- code/src/components/NextAuthClientSessionProvider.js
+++ code/src/components/NextAuthClientSessionProvider.js
@@ -1,7 +1,14 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider, useSession } from 'next-auth/react'
 
 export default function NextAuthClientSessionProvider({ children }) {
   return <SessionProvider>{children}</SessionProvider>
 }
+
+// TODO: Nanti buatin persisten auth guard untuk halaman / component yang harus ada user/session nya
+// function PersistentAuthGuard() {
+//   const session = useSession()
+
+//   if (!!session.status)
+// }
