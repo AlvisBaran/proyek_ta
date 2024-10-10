63a3edee461d73476043a2364f65dea52390e679
avonleira <alvisbaran00@gmail.com>
2024-10-10 10:30:55 +0700

    Update config.js

--- code/src/minio/config.js
+++ code/src/minio/config.js
@@ -5,8 +5,8 @@ export const mainBucketName = process.env.NEXT_PUBLIC_MINIO_BUCKET
 export const minioClient = new Client({
   endPoint: process.env.NEXT_PUBLIC_MINIO_ENDPOINT ?? 'play.min.io',
   port: !!process.env.NEXT_PUBLIC_MINIO_PORT ? Number(process.env.NEXT_PUBLIC_MINIO_PORT) : 9000,
-  useSSL: false,
-  // useSSL: process.env.NODE_ENV === 'production',
+  // useSSL: false,
+  useSSL: process.env.NODE_ENV === 'production',
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY
 })
